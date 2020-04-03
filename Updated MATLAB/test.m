58ec3586997d4c58c8895010d1deacaa0fd93707
Willy Chang <willychang@rocketmail.com>
2020-04-02 17:52:00 -1000

    update: test.m

--- Updated MATLAB/test.m
+++ Updated MATLAB/test.m
@@ -1,12 +1,15 @@
-path = 'C:\Users\wchang\Desktop\VIP\Code\testOne\';%Insert folder path here
+path = 'C:\Users\wchang\Desktop\VIP\Code\testOne\'; %Insert folder path here
+
+size = 496;
+bitdepth = uint8;
 files = dir(strcat(path,'/*.png'));
 for i=1:size(files)
     Image = imread(strcat(path, files(i).name));
     dim = size(Image);
-    if(dim(1) ~= 496 || dim(2) ~= 496)
-        if(class(Image) ~= uint8)
+    if(dim(1) ~= size || dim(2) ~= size)
+        if(class(Image) ~= bitdepth)
            disp(strcat('Wrong bit depth: ', files(i).name)); 
         end
-      disp(files(i).name);  
+      disp(strcat('Wrong size: ', files(i).name));  
     end
 end
