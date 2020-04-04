573a1e3b4b2c578de63d1bd4b333f03defccfe89
Christine Ramos <christineramos12@gmail.com>
2020-04-03 15:56:57 -1000

    NetworkTraining.py using Pytorch

---
+++ Python/NetworkTraining2.py
@@ -0,0 +1,10 @@
+# Network Training script that mainly utilizes PyTorch
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as functional
+
+class Net(nn.Module):   # Define neural network
+    def __init__(self):
+        super(Net, self).__init__()
+        
